0f65d5ebdc1eef2c753946eaef67f394d7fac5bb
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-04 00:36:06 +0300

    feat: Add mobile navigation to Navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -52,6 +52,74 @@ function Navbar() {
           </div>
         </Link>
       </div>
+      {/* mobile navigation */}
+      <div className="flex sm:hidden justify-between items-center relative">
+        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+          <img
+            src={thirdweb}
+            alt="user"
+            className="w-[60%] h-[60%] object-contain"
+          />
+        </div>
+        <img
+          src={menu}
+          alt="menu"
+          className="w-[34px] h-[34px] object-contain cursor-pointer"
+          onClick={() => setToggleDrawer(!toggleDrawer)}
+        />
+        <div
+          className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-md ${
+            !toggleDrawer ? "-translate-y-[100vh]" : "translate-y-0"
+          } transition-all duration-700`}
+        >
+          <ul className="mb-4">
+            {navlinks.map((link) => (
+              <li
+                key={link.name}
+                className={`flex p-4 ${
+                  isActive === link.name && "bg-[#3a3a43]"
+                }`}
+                onClick={() => {
+                  setIsActive(link.name);
+                  setToggleDrawer(false);
+                  navigate(link.link);
+                }}
+              >
+                <img
+                  src={link.imgUrl}
+                  alt={link.name}
+                  className={`w-[24px] h-[24px] object-contain ${
+                    isActive === link.name ? "grayscale-0" : "grayscale"
+                  }`}
+                />
+                <p
+                  className={`ml-[20px] kanit-semibold text-[14px] ${
+                    isActive === link.name
+                      ? "text-[#1dc071]"
+                      : "text-[#a0a0191]"
+                  }`}
+                >
+                  {link.name}
+                </p>
+              </li>
+            ))}
+          </ul>
+          <div className="flex mx-4 mb-4">
+            <CustomButton
+              btnType="button"
+              address={address}
+              title={address ? "Create a campaign" : "Connect"}
+              onClick={() => {
+                if (address) {
+                  navigate("/create-campaign");
+                } else {
+                  navigate("/connect");
+                }
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
