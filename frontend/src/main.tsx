9637bb17d5f83647bed9030034cf78fcc9ce82c2
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-03 21:55:38 +0300

    chore: add assets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { ThirdwebProvider } from "thirdweb/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ThirdwebProvider>
+  <ThirdwebProvider>
+    <Router>
       <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
+    </Router>
+  </ThirdwebProvider>
 );
