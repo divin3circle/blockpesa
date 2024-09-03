26bddf58ca97e446782f9214ecf52601049c55f5
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-03 23:36:32 +0300

    chore: Add CustomButton component and update Navbar

---
+++ frontend/src/components/CustomButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CustomButton() {
+  return (
+    <div>
+      <p>custom</p>
+    </div>
+  );
+}
+
+export default CustomButton;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import CustomButton from "./CustomButton";
+import logo from "../assets/logo2.png";
+import menu from "../assets/menu.svg";
+import search from "../assets/search.svg";
+import thirdweb from "../assets/thirdweb.png";
+import { navlinks } from "../constants";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("dashboard");
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+  return (
+    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24]">
+        <input
+          placeholder="Search for campaigns"
+          className="flex w-full font-normal text-white outline-none bg-transparent placeholder:text-[#4b5264] text0[14px]"
+        />
+        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
+          <img
+            src={search}
+            alt="search"
+            className="w-[15px] h-[15px] object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
