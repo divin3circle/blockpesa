60ac268bcbc6a272ce6dda2742eb44d9107c9fc2
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-04 12:04:30 +0300

    feat: Update getCampaigns function to return all campaigns

--- web3/contracts/Crowdfunding.sol
+++ web3/contracts/Crowdfunding.sol
@@ -59,5 +59,15 @@ contract CrowdFunding {
     }
 
 
-    function getCampaigns() {}
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint256 i = 0; i < numberOfCampaigns; i++){
+           Campaign storage item = campaigns[i];
+
+           allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
 }
