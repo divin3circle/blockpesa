1c744ddb3494362c3d6f37fc8d6ecc4497eba716
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-04 12:42:32 +0300

    chore: Update web3 package.json scripts

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -33,6 +33,14 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: "sepolia",
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: "https://eth-sepolia.api.onfinality.io/public",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
