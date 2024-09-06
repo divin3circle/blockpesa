12d2549ff2da86907e3a8bed481df96e1121bb64
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-06 15:01:44 +0300

    refactor: Refactor token generation logic in token.js

--- daraja/controllers/token.js
+++ daraja/controllers/token.js
@@ -1,32 +1,32 @@
 const axios = require("axios");
 
-const createToken = async (res, req, next) => {
-  const secret =
-    process.env.SECRET_KEY |
-    "ORWQpZ3S57Z9bASbpvzymGk886g6dflKYZ200IpZ8ejSH3zIWY1AFqsxeWk5r2vC";
-  const consumer =
-    process.env.CONSUMER_KEY |
-    "BkoPvdZkGsedoAiUWKAUJKGGBGKWLrpmVcDUWO6NIA2YnMB8";
-  const auth = new Buffer.from(`${consumer}:${secret}`).toString("base64");
+const client_id = "BkoPvdZkGsedoAiUWKAUJKGGBGKWLrpmVcDUWO6NIA2YnMB8";
+const client_secret =
+  "ORWQpZ3S57Z9bASbpvzymGk886g6dflKYZ200IpZ8ejSH3zIWY1AFqsxeWk5r2vC";
+const auth = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
 
-  await axios
-    .get(
-      "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+const createToken = async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://sandbox.safaricom.co.ke/oauth/v1/generate",
       {
+        params: {
+          grant_type: "client_credentials",
+        },
         headers: {
-          authorization: `Basic ${auth}`,
+          Authorization: `Basic ${auth}`,
+          "Content-Type": "application/x-www-form-urlencoded",
         },
       }
-    )
-    .then((data) => {
-      console.log(data.data);
-      next();
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    );
+    console.log(response.data);
+    // res.json(response.data);
+  } catch (error) {
+    console.error(
+      "Error creating token:",
+      error.response ? error.response.data : error.message
+    );
+  }
 };
 
-module.exports = {
-  createToken,
-};
+module.exports = { createToken };
