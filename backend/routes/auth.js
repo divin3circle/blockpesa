d5fa90b5cdc2a07872e48361f1e1240d8cdfefda
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-08 00:20:02 +0300

    ressolve conflicts with feat/server

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getAuthRequest, handleCallback } = require("../controller/authController");
-
-const router = express.Router();
-
-router.get("/sign-in", getAuthRequest);
-router.post("/callback", handleCallback);
-
-module.exports = router;
