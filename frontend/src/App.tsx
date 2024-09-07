856d55446a7fcf154a7749d0c3c0880fdfc2cf1a
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-07 15:34:07 +0300

    feat: Add react-hot-toast and react-router-dom dependencies
    Add react-hot-toast and react-router-dom dependencies to the package.json file in the frontend directory.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import { Navbar, Sidebar } from "./components";
 import { CampaignDetails, CreateCampaign, Profile, Home } from "./pages";
 import { Toaster } from "react-hot-toast";
+import GetUserInfo from "./pages/GetUserInfo";
+
 export function App() {
   return (
     <main className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
@@ -13,7 +15,8 @@ export function App() {
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/get-user-info" element={<GetUserInfo />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/create-campaign" element={<CreateCampaign />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-details/:id" element={<CampaignDetails />} />
---
+++ frontend/src/pages/GetUserInfo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GetUserInfo() {
+  return (
+    <div>
+      <h1>bwbck3bocnon3o</h1>
+    </div>
+  );
+}
+
+export default GetUserInfo;
