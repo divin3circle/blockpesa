bf23f01cf2721ba51d3eb49bee14c27656dd7268
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-07 15:38:43 +0300

    refactor: Update sidebar display logic in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { Navbar, Sidebar } from "./components";
 import { CampaignDetails, CreateCampaign, Profile, Home } from "./pages";
 import { Toaster } from "react-hot-toast";
 import GetUserInfo from "./pages/GetUserInfo";
 
 export function App() {
+  const location = useLocation();
+  const currentPath = location.pathname;
   return (
     <main className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 relative">
-        <Sidebar />
+        {currentPath === "get-user-info" && <Sidebar />}
       </div>
 
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        <Navbar />
+        {currentPath === "get-user-info" && <Navbar />}
         <Routes>
           <Route path="/get-user-info" element={<GetUserInfo />} />
           <Route path="/home" element={<Home />} />
